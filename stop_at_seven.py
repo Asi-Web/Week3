f2ef36eea6eab83cf013629fd284d23aed19ab82
Asi-Web <agblezea@students.trinitydc.edu>
2026-02-22 20:56:47 -0500

    Add loop that stops on number divisible by 7

---
+++ stop_at_seven.py
@@ -0,0 +1,17 @@
+## Asiwome Agbleze
+## CMSC 1111
+## Soring 2026
+## Assignment Loop from 1 to 100
+
+for number in range(1, 101):
+    # DO: ADD divisibility check here
+    pass
+for number in range(1, 101):
+    # Check if number is divisible by 7
+    if number % 7 == 0:
+        break
+for number in range(1, 101):
+    if number % 7 == 0:
+        print("First number divisible by 7 is:", number)
+        break   
+
